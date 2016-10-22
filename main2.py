7fa163a8d94ee0c9f6e7777ffea3bd1ba29fd322
akhilles <akhilvelagapudi@gmail.com>
2016-10-22 16:40:29 -0400

    finding moves from differences in matrices

--- main2.py
+++ main2.py
@@ -8,20 +8,48 @@ class Pieces:
 
 w = 8
 h = 8
-Matrix = [[0 for x in range(w)] for y in range(h)]
 
 def initialize(matrix):
-    matrix[0][0] = Pieces.WHITE
+    for f in range(0,8):
+        for r in range(0,2):
+            matrix[r][f] = Pieces.white
+        for r_b in range(6,8):
+            matrix[r_b][f] = Pieces.black
     return matrix
 
 def print_board(matrix):
     for rank in reversed(matrix):
         print rank
+    print
 
-for f in range(0,8):
-    for r in range(0,2):
-        Matrix[r][f] = Pieces.white
-    for r_b in range(6,8):
-        Matrix[r_b][f] = Pieces.black
+def get_differences(matrix_old, matrix_new):
+    differences = []
+    for rank in range(0,8):
+        for filee in range(0,8):
+            if (matrix_old[rank][filee] != matrix_new[rank][filee]):
+                differences.append((rank, filee))
+    return differences
 
-print_board(Matrix)
+def compare(matrix_old, matrix_new):
+    differences = get_differences(matrix_old, matrix_new)
+    length = len(differences)
+    if (length == 2):
+        if (matrix_new[differences[0][0]][differences[0][1]] != 0):
+            return(differences[1],differences[0])
+        else:
+            return(differences[0],differences[1])
+    return 0
+
+Matrix1 = [[0 for x in range(w)] for y in range(h)]
+Matrix2 = [[0 for x in range(w)] for y in range(h)]
+
+Matrix1 = initialize(Matrix1)
+Matrix2 = initialize(Matrix2)
+
+Matrix2[3][3] = Pieces.white
+Matrix2[1][3] = Pieces.empty
+
+print_board(Matrix1)
+print_board(Matrix2)
+print get_differences(Matrix1, Matrix2)
+print compare(Matrix1, Matrix2)
